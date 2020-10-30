041499a63871f5f7904b4295b605e75b499cfedb
JacopoMattiaMarconi <73340924+JacopoMattiaMarconi@users.noreply.github.com>
2020-10-30 13:20:13 +0100

    Primo commit made succesfully

--- src/STcpEchoUpper.java
+++ src/STcpEchoUpper.java
@@ -3,7 +3,6 @@ import java.io.*;
 import java.net.*;
 
 public class STcpEchoUpper {
-
     public static void main(String[] args) throws IOException {
         String pagina="<!DOCTYPE html>\n" +
                 "<html>\n" +
